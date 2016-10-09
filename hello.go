4c0403a50d6539de20131d7f1ceeba05d59e0575
Rushworth, Oliver <oliver.rushworth@sky.uk>
2016-10-09 14:16:59 +0100

    [Refactor] - Change Printf so it uses the os.Stdout writer

--- hello.go
+++ hello.go
@@ -3,9 +3,10 @@ package gotutorial
 import (
   "fmt"
   "net/http"
+  "os"
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello World")
-  fmt.Printf("Request from: %s \n", r.RemoteAddr)
+  fmt.Fprintf(os.Stdout ,"Request from: %s \n", r.RemoteAddr)
 }
